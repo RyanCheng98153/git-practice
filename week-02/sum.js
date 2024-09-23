78318074ad41663e502e2c47e05edfc8b514bda1
RyanCheng98153 <ryan98153@gmail.com>
2024-09-24 02:01:24 +0800

    feat: sum testing

--- week-02/sum.js
+++ week-02/sum.js
@@ -1,10 +1,3 @@
-function test( func ){
-    // a reusable testing function
-    console.time(`function ${func.name}()`);
-    sum = func([1, 5, 3, 2]); // 11
-    console.timeEnd(`function ${func.name}()`);
-}
-
 // ary: number array
 function sumForloop(ary) {
 	// TODO: sum all elements in ary
@@ -17,24 +10,64 @@ function sumForloop(ary) {
 
 function sumMap(ary) {
 	let sum = 0;
-    arr.map(val => sum += val);
+    ary.map(val => sum += val);
     return sum;
 }
 
-
 function sumReduce(ary) {
-	return ary.map((curVal) => sum + curVal, 0)
+	return ary.reduce((sum, curVal) => sum + curVal, 0)
 }
 
 function sumRecursive(arr) {
     if (arr.length === 0) return 0;
-    return arr[0] + sumArray(arr.slice(1));
+    return arr[0] + sumRecursive(arr.slice(1));
   }
 
-for ( const x of Array(5).keys() ){
-    test(sumForloop)
-    test(sumMap)
-    test(sumForloop)
-    test(sumArray)
+// TODO: 每次都是從 1 加到 10000，這樣的測試結果準確嗎？不同的程式語言結果一樣嗎？
+//       OS 在 CPU 的處理上，會不會有 cache?
+// TODO: 換成小一點的數字，結果如何？ 
+
+function test( func, value ){
+    // a reusable testing function
+    var start = performance.now();
+    let sum = func(value); // 11
+    var end = performance.now();
+    console.log(end - start)
+    return end - start;
 }
+
+let time = [
+    {"map": 0},
+    {"reduce": 0},
+    {"forloop": 0},
+    {"recursive": 0}
+]
+
+for (const iter of Array(1).keys() ){
+    let val = test(sumMap, [10, 2, 102, 43])
+    console.log(val)
+}
+
+let iters = 100
+
+for ( const x of Array(3).keys() ){
+    let value = [...Array(10000).keys()].map(i => i + 1);
+    if ( true ){
+        value = [...Array(10000).keys()].map(i => i + 1);
+    }
     
+    time["map"] += test(sumMap, value)
+    time["reduce"] += test(sumReduce, value)
+    time["forloop"] += test(sumForloop, value)
+    // time['recursive'] += test(sumRecursive, value)
+}
+
+console.log( `${time['map'] / iters} (${time['map']} / ${iters})` )
+console.log( `${time['reduce'] / iters} (${time['reduce']} / ${iters})` )
+console.log( `${time['forloop'] / iters} (${time['forloop']} / ${iters})` )
+// console.log( `${time['recursive'] / iters} (${time['recursive']} / ${iters})` )
+
+/* 筆記：
+*   當執行 10000 次 recursive 時，maximum out of recursive 
+* 
+*/
